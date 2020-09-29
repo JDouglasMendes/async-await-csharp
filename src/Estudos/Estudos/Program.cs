589b45ee55add10b2ad68bb357264f81c5768532
Douglas Mendes <douglasaleixomendes@gmail.com>
2020-09-29 13:16:52 -0300

    Exemple Task.Factory not wait execution

---
+++ src/Estudos/Estudos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Estudos
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            ParrellForTest();
+            await Test1();
+            Console.Read();
+        }       
+
+
+        private  static void ParrellForTest()
+        {
+            var result = Parallel.For(1, 10, async (i) =>
+            {
+                await Task.Delay(i / 2 * 1000);
+                Console.WriteLine(i);
+            });
+           
+        }
+        
+        /// <summary>
+        /// Rodou
+        /// Com await
+        /// Hello World!
+        /// sem await
+        /// </summary>
+        /// <returns></returns>
+        private static async Task Test1()
+        {
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                Console.WriteLine("Rodou");
+            });
+
+            await TestAsync(6000, "com await");
+
+            var t2 = TestAsync(500, "sem await");
+
+            await Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(8000);
+                Console.WriteLine("Factory");
+            });
+            
+            Console.WriteLine("Hello World!");
+
+            Task.WaitAll(t2);
+        }
+
+        private static async Task TestAsync(int time,  string message)
+        {
+            await Task.Delay(time);
+            Console.WriteLine(message);
+        }
+    }
+}
